6d7dc0a9d4e37a5737931f490c50323509ca62a9
Physics-Computer <ms_0710_@naver.com>
2024-11-02 01:47:28 +0900

    header, title, message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,53 @@
 // 아래의 import문으로 next/image의 Image 컴포넌트를 사용할 수 있습니다.
-// import Image from "next/image"
+import Image from "next/image"
 
 // 아래의 import문으로 MessageBox 컴포넌트를 사용할 수 있습니다.
-//import MessageBox from "@/components/MessageBox";
+import MessageBox from "@/components/MessageBox";
 
 export default function Home() {
+  const mainstyle = {
+    backgroundColor: '#FFDDE3',
+    display: 'flex',
+    minHeight: '100vh',
+    flexDirection: 'column',
+    alignItems: 'center', 
+  };
+
+  const titleStyle = {
+    padding: '147px 0px 0px 0px',
+  };
+
+  const sentenceStyle = {
+    padding: '58px',
+  };
+
+  const buttonStyle = {
+    display: 'flex',
+    width: '678px',
+    height: '93px',
+    backgroundColor: '#B61111',
+    borderRadius: '12px',
+    color: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+
+
+
   return (
-    <main>
-      {/* font 적용하는 법 예시 */}
-      <h1 className="font-['Changwon']"></h1>
+    <main style={mainstyle}>
+      <div style={titleStyle}>
+        <h1 className="text-[120px] font-['Changwon']">연말 편지 우체통</h1>
+      </div>
+
+      <div style={sentenceStyle}>
+        <h2 className="text-[36px] font-['Hakgyoansim']">💌  김민수님의 연말 편지 우체통으로 00건의 편지가 도착했어요!</h2>
+      </div>
+
+      <div style={buttonStyle}>
+        <h2 className="text-[36px] font-['Hakgyoansim']">편지 쓰러 가기</h2>
+      </div>
+
     </main>
   );
 }
